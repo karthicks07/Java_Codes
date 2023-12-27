820dc40dd25d41cf44bb31c66cc28b8604b8d90c
Karthick_Senthil <113781335+karthicks07@users.noreply.github.com>
2023-12-27 23:37:57 +0530

    Create 1.Typecasting.java

---
+++ 1.Typecasting.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter the float value - ");
+		float f = s.nextFloat();
+		System.out.print("Enter the integer value - ");
+		int n = s.nextInt();
+		int n1 = (int)f;//Typecasting
+		float f1 = n; 
+		System.out.printf("After converting float to integer : %d\n",n1);
+		System.out.printf("After converting integer to float : %.3f",f1);
+		
+	}
+}
