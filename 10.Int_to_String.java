52ff6c94f71354b03c0d6e943a33b3455623f879
Karthick_Senthil <113781335+karthicks07@users.noreply.github.com>
2024-01-01 22:50:02 +0530

    Create 10.Int_to_String.java

---
+++ 10.Int_to_String.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.print("Enter the integer - ");
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		String str = String.valueOf(n);
+		System.out.printf("String : %s",str);
+	}
+}
